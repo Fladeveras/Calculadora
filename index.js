a6338f37410b917461d3329dce267bf7bcbc8017
Flavya <flavyamarya007@gmail.com>
2023-08-22 17:24:21 -0300

    Atualizaçã

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const teclasRes=document.querySelector(".res")
 const teclasD=document.querySelector(".display")
 const ton=document.getElementById("ton")
 const tlimpar=document.getElementById("tlimpar")
+const tigual=document.getElementById("tigual")
 
 let sinal=false
 let decimal=false
@@ -11,15 +12,19 @@ let decimal=false
 teclasNum.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
         sinal=false
-        if(teclasD.innerHTML=="0"){
-            teclasD.innerHTML=""
-        }
         if(evt.target.innerHTML=="."){
             if(!decimal){
            decimal=true
-           teclasD.innerHTML+=evt.target.innerHTML
+           if(teclasD.innerHTML=="0"){
+           teclasD.innerHTML="0."
+        }else{
+            teclasD.innerHTML+=evt.target.innerHTML
+        }
         }
         }else{
+            if(teclasD.innerHTML=="0"){
+                teclasD.innerHTML=""
+            }
         teclasD.innerHTML+=evt.target.innerHTML
         console.log (evt)
     }
@@ -35,7 +40,7 @@ teclasOp.forEach((el)=>{
             teclasD.innerHTML=""
         }
         if(evt.target.innerHTML=="x"){
-            teclasD.innerHTML+="×"
+            teclasD.innerHTML+="x"
         }else{
             teclasD.innerHTML+=evt.target.innerHTML
         }
@@ -48,6 +53,13 @@ tlimpar.addEventListener ("click",(evt)=>{
     decimal=false
     teclasD.innerHTML="0"
     
+})
+
+tigual.addEventListener ("click",(evt)=>{
+    sinal=false
+    decimal=false
+   const res=eval(teclasD.innerHTML)
+   teclasD.innerHTML=res
 })
 
 
